9874ef0d6755db0aaa9841370fb1a7e5f966227f
Wladimir Bandeira <wladimir.bandeira@capgemini.com>
2021-04-20 09:27:46 -0300

    encrypting persistent password in the database

--- backend/api/user.js
+++ backend/api/user.js
@@ -1,6 +1,11 @@
+const bcrypt = require('bcrypt-nodejs')
+
 module.exports = app => {
-    const save = (req, res) => {
-        res.send('user salve')
+    const { existsOrError, notExistsOrError, equalsOrError } = app.api.validation
+
+    const encryptPassword = password => {
+        const salt = bcrypt.genSaltSync(10)
+        return bcrypt.hashSync(password, salt)
     }
     return { save }
 }
