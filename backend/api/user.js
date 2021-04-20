5407b12c0de9f68070c15a34553079e2c6c880b4
Wladimir Bandeira <wladimir.bandeira@capgemini.com>
2021-04-20 09:33:23 -0300

    validate the creation of the user at the moment he registers

--- backend/api/user.js
+++ backend/api/user.js
@@ -7,5 +7,27 @@ module.exports = app => {
         const salt = bcrypt.genSaltSync(10)
         return bcrypt.hashSync(password, salt)
     }
-    return { save }
+
+    const save = async (req, res) => {
+        const user = { ...req.body }
+        if (req.params.id) user.id = req.params.id
+
+        try {
+            existsOrError(user.name, 'Nome não informado')
+            existsOrError(user.email, 'E-mail não informado')
+            existsOrError(user.password, 'Senha não informada')
+            existsOrError(user.confirmPassword, 'Confirmação de Senha inválida')
+            equalsOrError(user.password, user.confirmPassword,
+                'Senhas não conferem')
+
+            const userFromDB = await app.db('users')
+                .where({ email: user.email }).first()
+            if (!user.id) {
+                notExistsOrError(userFromDB, 'Usuário já cadastrado')
+            }
+        } catch (msg) {
+            return res.status(400).send(msg)
+        }
+    }
+        return { save }
 }
