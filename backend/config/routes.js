f143b759048abf782db83d34de95b8f096436751
Wladimir Bandeira <wladimir.bandeira@capgemini.com>
2021-04-20 10:04:21 -0300

    creating route for update

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -2,4 +2,7 @@ module.exports = app => {
     app.route('/users')
         .post(app.api.user.save)
         .get(app.api.user.get)
+    
+    app.route('/users/:id')
+        .put(app.api.user.save)
 }
